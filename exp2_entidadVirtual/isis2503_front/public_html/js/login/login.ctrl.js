3eb042d0c31dade1f71b0100d9984303da01cd84
VIVIAN NATALIA GOMEZ CUBILLOS <vn.gomez@sis.virtual.uniandes.edu.co>
2018-05-16 20:38:41 -0500

    proyecto web fullHD4K

---
+++ exp2_entidadVirtual/isis2503_front/public_html/js/login/login.ctrl.js
@@ -0,0 +1,14 @@
+(function (ng) {
+    var mod = ng.module("loginModule");
+    mod.controller('loginCtrl', ['$scope', '$http', '$state', '$rootScope',
+        function ($scope, $http, $state, $rootScope) {
+
+            $scope.user = {};
+            $scope.data = {};
+
+
+        }
+    ]);
+}
+)(window.angular);
+
