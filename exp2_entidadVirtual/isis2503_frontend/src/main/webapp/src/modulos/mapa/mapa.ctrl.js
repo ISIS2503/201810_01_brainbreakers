8b143ef2e33410d27647d8fb73de81dab561ac4a
VIVIAN NATALIA GOMEZ CUBILLOS <vn.gomez@sis.virtual.uniandes.edu.co>
2018-05-17 14:52:16 -0500

    arreglos dashboard

--- exp2_entidadVirtual/isis2503_frontend/src/main/webapp/src/modulos/mapa/mapa.ctrl.js
+++ exp2_entidadVirtual/isis2503_frontend/src/main/webapp/src/modulos/mapa/mapa.ctrl.js
@@ -1,20 +1,56 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 (function (ng) {
     var mod = ng.module("mapaModule");
-    mod.controller('mapaCtrl', ['$scope', '$http', '$state', '$rootScope',
-        function ($scope, $http, $state, $rootScope) {
+    mod.constant("mapaContext", "api/divisiones");
+    mod.controller('mapaCtrl', ['$scope', '$http', 'mapaContext', '$state', '$filter',
+        function ($scope, $http, mapaContext, $state, $filter) {
+            $http.get(mapaContext).then(function (response) {
+                $scope.bodasRecords = response.data;
+            });
+            
+            if ($state.params.bodaId !== undefined) {
+                $http.get(mapaContext + '/' + $state.params.bodaId).then(function (response) {
+                   
+                    $scope.currentBoda = response.data;
+                    $scope.currentFecha = $scope.currentBoda.fecha;
+                   
+                        
+                    var estadoDetail = 'bodaDetail';
+                    if ($state.current.name === estadoDetail) {
+                        
+                        try {
+                             var fechita = $filter('date')($scope.currentBoda.fecha, 'medium', '');
+                            var countDownDate = new Date(fechita).getTime();
+                            var x = setInterval(function () {
+                                if($state.current.name !== estadoDetail )
+                                {
+                                    clearInterval(x);
+                                }                           
+                               
+                                var now = new Date().getTime();
+                                var distance = countDownDate - now;
+                                var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-            $scope.user = {};
-            $scope.data = {};
+                                document.getElementById("demo").innerHTML = days + "días " + hours + "horas " + minutes + "minutos " + seconds + "s ";
+
+                                if (distance < 0) {
+                                    clearInterval(x);
+                                    document.getElementById("demo").innerHTML = "Aún puedes aplazar la fecha";
+                                }
+                                
+                            }, 1000);
+                        } catch (err) {
+                        }
+                    }
+                });
 
 
-        }
-    ]);
-}
-)(window.angular);
+            }
+
+        }]);
+})(angular);
+
 
 
